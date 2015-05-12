241e213a1aa1e425f4dc8e43a2c0c12245a8200d
Anthony Gelibert <anthony.gelibert@me.com>
2015-05-12 22:49:31 +0200

    Game.java: Global review (lots of TODO).

--- src/fr/iutvalence/virgilbontempi/connectfour/Game.java
+++ src/fr/iutvalence/virgilbontempi/connectfour/Game.java
@@ -3,6 +3,7 @@ package fr.iutvalence.virgilbontempi.connectfour;
 import java.util.Objects;
 import java.util.Scanner;
 
+/* TODO No "toString()"? */
 /**
  * Game: the players and the grid.
  *
@@ -15,9 +16,9 @@ public class Game {
     /** Attribute named "player2" type "Player". */
     private final Player player2;
     /** Attribute named "grid" type "Grid". */
-    private final Grid grid;
+    private final Grid   grid;
     /** Attribute named "currentPlayer" type "Player". */
-    private Player currentPlayer;
+    private       Player currentPlayer;
 
     /** Create a new game for the two given players. */
     public Game(final String name1, final String name2) {
@@ -36,91 +37,99 @@ public class Game {
      * Starter function.
      */
     public void start() {
-        System.out.println(this.grid);
-        while ((this.win() == false) && (this.allColumnAreFull() == false)) {
-            this.playARound(currentPlayer);
-            this.switchPlayer();
+        System.out.println(grid);
+
+        while (!win() && !areAllColumnFull()) {
+            playARound(currentPlayer);
+            switchPlayer();
         }
-        if (this.win()) {
-            this.switchPlayer();
-            System.out.println(this.currentPlayer + ", you won !!! Congratulations");
+
+        /* TODO This second call to "win" is costly! */
+        if (win()) {
+            switchPlayer();
+            System.out.println(currentPlayer + ", you won !!! Congratulations");
         }
         else {
             System.out.println("Nobody has won");
         }
     }
 
-    /**
-     * Play a round.
-     */
+    /** Play a round. */
     private void playARound(Player currentPlayer) {
         try {
             int choosenColumn = inputColumn();
-            this.placementPiece(choosenColumn);
-            System.out.println(this.grid);
+            placementPiece(choosenColumn);
+            System.out.println(grid);
         }
-        catch (OutOfRangeException e) {
+        catch (OutOfRangeException ignore) {
             System.out.println("Input an other column, the selected column is out of range");
             playARound(currentPlayer);
         }
-        catch (FullColumnException f) {
-            System.out.println("Input an other column, the selectedcolumn is full");
+        catch (FullColumnException ignore) {
+            System.out.println("Input an other column, the selected column is full");
             playARound(currentPlayer);
         }
     }
 
+    /* TODO This kind of javadoc is completly useless! */
     /**
      * Function input column.
      */
     public int inputColumn() throws OutOfRangeException, FullColumnException {
+        /* TODO You should avoid to create a new Scanner in each call to this method. */
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input a column");
         int column = scanner.nextInt();
         if ((column < Grid.NBCOLUMN) || (column > Grid.NBCOLUMN)) {
             throw new OutOfRangeException();
         }
-        if (columnIsFull(column)) {
+        if (isColumnFull(column)) {
             throw new FullColumnException();
         }
         return column;
     }
 
+    /* TODO Translate. */
+    /* TODO This method has to be moved to Grid. Why do you put it here??? */
     /**
      * Function piece's placement.
      */
     public void placementPiece(int column) {
         int line = 0;
-        while ((line < Grid.NBLINE) && (this.grid.getGrille()[line][column] != Piece.EMPTY)) {
+        while ((line < Grid.NBLINE) && (grid.getGrille()[line][column] != Piece.EMPTY)) {
             line++;
         }
-        if (currentPlayer == this.player1) {
-            this.grid.setGrille(line - 1, column, Piece.RED);
+        /* TODO Why this test when the only one caller (playARound()) has already the information? */
+        if (Objects.equals(currentPlayer, player1)) {
+            grid.setGrille(line - 1, column, Piece.RED);
         }
         else {
-            this.grid.setGrille(line - 1, column, Piece.YELLOW);
+            grid.setGrille(line - 1, column, Piece.YELLOW);
         }
     }
 
+    /* TODO This method has to be moved to Grid. Why do you put it here??? */
     /**
      * Function checking column.
      */
-    public boolean columnIsFull(int column) {
-        if (this.grid.getGrille()[0][column] != Piece.EMPTY) { return true; }
-        return false;
+    public boolean isColumnFull(int column) {
+        return this.grid.getGrille()[0][column] != Piece.EMPTY;
     }
 
+    /* TODO This method has to be moved to Grid. Why do you put it here??? */
     /**
      * Function all columns are full.
      */
-    public boolean allColumnAreFull() {
+    public boolean areAllColumnFull() {
         for (int column = 0; column < Grid.NBCOLUMN; column++) {
-            if (this.columnIsFull(column) == false) {
+            if (!isColumnFull(column)) {
                 return false;
             }
         }
         return true;
     }
 
+    /* TODO This method has to be moved to Grid. Why do you put it here??? */
     /**
      * Function searching for 4 aligned pieces.
      */
@@ -134,57 +143,62 @@ public class Game {
                     }
                 }
                 catch (EmptyPieceException e) {
+                    /* TODO Nothing ??? */
                 }
             }
         }
         return false;
     }
 
+    /* TODO This method has to be moved to Grid. Why do you put it here??? */
     /**
      * Function searching for 4 horizontal right aligned pieces.
      */
     private boolean horizontalRight(int line, int column) throws EmptyPieceException {
-        if (this.grid.getGrille()[line][column] != Piece.EMPTY) {
+        if (grid.getGrille()[line][column] != Piece.EMPTY) {
             throw new EmptyPieceException();
         }
-        return (this.grid.getGrille()[line][column] == this.grid.getGrille()[line][column + 1]) &&
-                (this.grid.getGrille()[line][column + 2] == this.grid.getGrille()[line][column + 3]) &&
-                (this.grid.getGrille()[line][column + 1] == this.grid.getGrille()[line][column + 2]);
+        return (grid.getGrille()[line][column] == grid.getGrille()[line][column + 1]) &&
+               (grid.getGrille()[line][column + 2] == grid.getGrille()[line][column + 3]) &&
+               (grid.getGrille()[line][column + 1] == grid.getGrille()[line][column + 2]);
     }
 
+    /* TODO This method has to be moved to Grid. Why do you put it here??? */
     /**
      * Function searching for 4 vertical down aligned pieces.
      */
     private boolean verticalDown(int line, int column) throws EmptyPieceException {
-        if (this.grid.getGrille()[line][column] == Piece.EMPTY) {
+        if (grid.getGrille()[line][column] == Piece.EMPTY) {
             throw new EmptyPieceException();
         }
-        return (this.grid.getGrille()[line][column] == this.grid.getGrille()[line - 1][column]) &&
-                (this.grid.getGrille()[line - 2][column] == this.grid.getGrille()[line - 3][column]) &&
-                (this.grid.getGrille()[line - 1][column] == this.grid.getGrille()[line - 2][column]);
+        return (grid.getGrille()[line][column] == grid.getGrille()[line - 1][column]) &&
+               (grid.getGrille()[line - 2][column] == grid.getGrille()[line - 3][column]) &&
+               (grid.getGrille()[line - 1][column] == grid.getGrille()[line - 2][column]);
     }
 
+    /* TODO This method has to be moved to Grid. Why do you put it here??? */
     /**
      * Function searching for 4 diagonal down right aligned pieces.
      */
     private boolean diagonalDownRight(int line, int column) throws EmptyPieceException {
-        if (this.grid.getGrille()[line][column] != Piece.EMPTY) {
+        if (grid.getGrille()[line][column] != Piece.EMPTY) {
             throw new EmptyPieceException();
         }
-        return (this.grid.getGrille()[line][column] == this.grid.getGrille()[line + 1][column + 1]) &&
-                (this.grid.getGrille()[line + 2][column + 2] == this.grid.getGrille()[line + 3][column + 3]) &&
-                (this.grid.getGrille()[line + 1][column + 1] == this.grid.getGrille()[line + 2][column + 2]);
+        return (grid.getGrille()[line][column] == grid.getGrille()[line + 1][column + 1]) &&
+               (grid.getGrille()[line + 2][column + 2] == grid.getGrille()[line + 3][column + 3]) &&
+               (grid.getGrille()[line + 1][column + 1] == grid.getGrille()[line + 2][column + 2]);
     }
 
+    /* TODO This method has to be moved to Grid. Why do you put it here??? */
     /**
      * Function searching for 4 diagonal down left aligned pieces.
      */
     private boolean diagonalDownLeft(int line, int column) throws EmptyPieceException {
-        if (this.grid.getGrille()[line][column] != Piece.EMPTY) {
+        if (grid.getGrille()[line][column] != Piece.EMPTY) {
             throw new EmptyPieceException();
         }
-        return (this.grid.getGrille()[line][column] == this.grid.getGrille()[line + 1][column - 1]) &&
-                (this.grid.getGrille()[line + 2][column - 2] == this.grid.getGrille()[line + 3][column - 3]) &&
-                (this.grid.getGrille()[line + 1][column - 1] == this.grid.getGrille()[line + 2][column - 2]);
+        return (grid.getGrille()[line][column] == grid.getGrille()[line + 1][column - 1]) &&
+               (grid.getGrille()[line + 2][column - 2] == grid.getGrille()[line + 3][column - 3]) &&
+               (grid.getGrille()[line + 1][column - 1] == grid.getGrille()[line + 2][column - 2]);
     }
 }
