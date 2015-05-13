eeda02cd78d7a90b0fd493101b19fff0ee7489de
VirgilBontempi <virgilbontempi@hotmail.fr>
2015-05-13 09:03:01 +0200

    commit pour l'erreur

---
+++ src/fr/iutvalence/virgilbontempi/connectfour/Starter.java
@@ -0,0 +1,12 @@
+package fr.iutvalence.virgilbontempi.connectfour;
+
+public class Starter
+{
+
+	public static void main(String[] args)
+	{
+		Game game = new Game("Toto", "Titi");
+		game.start();
+
+	}
+}
